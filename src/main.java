9e26a509803778824c7590db6007acafa823e171
HabibeY1 <habibeyavuz13@gmail.com>
2024-03-21 12:55:58 +0100

    Initial commit

---
+++ src/main.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class main {
+    public static void main(String[] args) throws IOException {
+        String str1;
+        boolean check = true;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println("The program checks if the word is palindrome:");
+        System.out.println("Enter a word to be checked:");
+        str1 = br.readLine().toLowerCase();
+
+
+        for (int i = 0; i < str1.length() / 2; i++) {
+            if (str1.charAt(i) != str1.charAt(str1.length() - i - 1)) {
+                System.out.println("It is not a palindrome.");
+                check = false;
+                break;
+            }
+        }
+
+
+        if (check) {
+            System.out.println(str1+"It is  a palindrome.");
+        } else {
+            System.out.println(str1+ "It is not a palindrome.");
+        }
+    }
+}
